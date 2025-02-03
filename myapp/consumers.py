84f892e1c99c17a83bfea3e12a842e3a84e2f92d
Wilson Morales <wilsonm57wm@gmail.com>
2025-02-03 17:20:19 -0600

    [update]: Cambio pe

--- myapp/consumers.py
+++ myapp/consumers.py
@@ -1,15 +1,43 @@
+# consumers.py
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from task.models import Task
+from asgiref.sync import sync_to_async
 
-class MiConsumer(AsyncWebsocketConsumer):
+class TaskConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.room_group_name = 'tasks'
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
-        await self.send(text_data=json.dumps({"message": "Conectado al WebSocket"}))
 
     async def disconnect(self, close_code):
-        pass
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
-        data = json.loads(text_data)
-        message = data.get("message", "Mensaje vacío")
-        await self.send(text_data=json.dumps({"message": f"Recibido: {message}"}))
+        text_data_json = json.loads(text_data)
+        action = text_data_json.get('action')
+
+        if action == 'create_task':
+            title = text_data_json.get('title')
+            if title:
+                # Crear la tarea en la base de datos
+                task = await sync_to_async(Task.objects.create)(title=title)
+                # Enviar la tarea creada a todos los clientes
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'task_created',
+                        'task': {
+                            'id': task.id,
+                            'title': task.title,
+                        },
+                    },
+                )
+
+    async def task_created(self, event):
+        # Enviar la tarea creada al cliente
+        task = event['task']
+        await self.send(text_data=json.dumps({
+            'action': 'task_created',
+            'task': task,
+        }))
--- myapp/routing.py
+++ myapp/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
-from .consumers import MiConsumer
+from .consumers import TaskConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/data/$", MiConsumer.as_asgi()),
+    re_path(r"ws/data/$", TaskConsumer.as_asgi()),
 ]
